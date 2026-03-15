a646b0966d879ebe623cff6f95b2ff5411d3be02
verifymzansi-a11y <verifymzansi@gmail.com>
2026-03-16 01:36:30 +0200

    Fix Cloudflare durable object deploy wrapper

--- workers/open-next-entry.mjs
+++ workers/open-next-entry.mjs
@@ -1,5 +1,8 @@
 import worker from "../.open-next/worker.js";
 
+export { DOQueueHandler } from "@opennextjs/cloudflare/durable-objects/queue";
+export { DOShardedTagCache } from "@opennextjs/cloudflare/durable-objects/sharded-tag-cache";
+export { BucketCachePurge } from "@opennextjs/cloudflare/durable-objects/bucket-cache-purge";
 export { RateLimiterDO } from "./rate-limiter.ts";
 
 export default worker;
